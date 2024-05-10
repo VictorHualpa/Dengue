e3ea8c2ba3c824769e23638b2957c0580f8a8415
Ramon Mina Ruiz <rmina@sunarp.gob.pe>
2024-05-10 14:03:42 -0500

    mapa de ubicacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,14 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("androidx.navigation.safeargs")
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
 }
 
 android {
-    namespace = "com.minsa.dengue"
+    namespace = "com.upc.myapplication"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.minsa.dengue"
+        applicationId = "com.upc.myapplication"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -34,28 +33,18 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation("androidx.annotation:annotation:1.6.0")
-    implementation("androidx.activity:activity:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 
-    // Voley
-    implementation ("com.android.volley:volley:1.2.1")
-
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
 }
--- app/src/main/java/com/minsa/dengue/global/Total.kt
+++
@@ -1,8 +0,0 @@
-package com.minsa.dengue.global
-
-class Total {
-    companion object{
-        const val rutaServicio  = "http://appdengue.somee.com/"
-        const val nroversion = "v.0.0.1"
-    }
-}
